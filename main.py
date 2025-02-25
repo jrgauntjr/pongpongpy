bc666e098ead2414228ad92d03a553af7ce42c91
jrgauntjr <joseph.r.gaunt@gmail.com>
2025-02-25 15:25:05 -0700

    Started making my structure of the project

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import arcade
 
+SCREEN_WIDTH, SCREEN_HEIGHT = arcade.get_display_size()
+SCREEN_TITLE = "Pong Pong"
 
+class PongPong(arcade.Window):
+    def __init__(self):
+        super().__init__()
+
+    def setup(self):
+        pass
+
+    def on_draw(self):
+        pass
 
 def main():
-    print("Hello World")
+    print(f"{SCREEN_WIDTH}x{SCREEN_HEIGHT}")
+
 
 
 
