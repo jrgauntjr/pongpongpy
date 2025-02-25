0807ccb77245c1bef335042eedaf6433b8ef6932
jrgauntjr <joseph.r.gaunt@gmail.com>
2025-02-25 15:37:20 -0700

    updated main function to get a working black screen

--- main.py
+++ main.py
@@ -3,18 +3,29 @@ import arcade
 SCREEN_WIDTH, SCREEN_HEIGHT = arcade.get_display_size()
 SCREEN_TITLE = "Pong Pong"
 
-class PongPong(arcade.Window):
+class PongPong(arcade.View):
     def __init__(self):
         super().__init__()
+        self.paused = False
 
     def setup(self):
-        pass
+        arcade.set_background_color(arcade.color.BLACK)
 
     def on_draw(self):
-        pass
+        self.clear()
+        
+    
+    def on_key_press(self, symbol, modifiers):
+        return super().on_key_press(symbol, modifiers)
 
 def main():
-    print(f"{SCREEN_WIDTH}x{SCREEN_HEIGHT}")
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.set_fullscreen(True)
+    game = PongPong()
+    game.setup()
+    
+    window.show_view(game)
+    arcade.run()
 
 
 
