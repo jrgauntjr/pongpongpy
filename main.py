01b1607f9b950a069d9d9c46e479634954c8992e
jrgauntjr <joseph.r.gaunt@gmail.com>
2025-02-26 14:38:26 -0700

    Got it to show a background as well as quitting and pausing fucntions working

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
 import arcade
+import os
 
 SCREEN_WIDTH, SCREEN_HEIGHT = arcade.get_display_size()
 SCREEN_TITLE = "Pong Pong"
 
-class PongPong(arcade.View):
-    def __init__(self):
-        super().__init__()
+class PongPong(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
         self.paused = False
+        self.background = None
+        self.setup()
 
     def setup(self):
-        arcade.set_background_color(arcade.color.BLACK)
+        self.background = arcade.load_texture("images/wall.png")
+        
+    
 
     def on_draw(self):
         self.clear()
-        
+        arcade.draw_texture_rect(self.background, arcade.LBWH(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT))
     
     def on_key_press(self, symbol, modifiers):
-        return super().on_key_press(symbol, modifiers)
+        if symbol == arcade.key.Q:
+            arcade.close_window()
+        elif symbol == arcade.key.P:
+            self.paused = not self.paused
+            if self.paused:
+                arcade.pause()
+            else:
+                arcade.start()
 
 def main():
-    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window = PongPong(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     window.set_fullscreen(True)
-    game = PongPong()
-    game.setup()
-    
-    window.show_view(game)
     arcade.run()
-
-
-
 
 if __name__ == "__main__":
     main()
